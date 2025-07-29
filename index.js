a8da90926db19339dc618ca77d2dee81f3ff3a2e
Kriticaaa22 <kritica523.be22@chitkara.edu.in>
2025-07-29 21:02:27 +0530

    Converted to Vercel serverless function

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
-const express= require('express');
-const app=express();
+module.exports = (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Only POST method allowed" });
+  }
 
-app.use(express.json());
-const PORT = process.env.PORT || 3000;
+  const data = req.body.data;
+  const even = [];
+  const odd = [];
+  const alphabets = [];
+  const specialChars = [];
+  let sum = 0;
+  let charArray = [];
 
-app.post('/bfhl',(req,res) =>{
-    const data=req.body.data;
-    const even=[];
-    const odd=[];
-    const alphabets=[];
-    const specialChars =[];
-    let sum=0;
-    let concat='';
-
-    for(let item of data){
-        const str=String(item);
-        if(!isNaN(str)){
-            const num=parseInt(str);
-            sum+=num;
-            (num%2 ==0? even :odd).push(str);
-
-        }
-        else if(/^[a-zA-Z]+$/.test(str)){
-            alphabets.push(str.toUpperCase());
-            concat += str;
-        }
-        else{
-            specialChars.push(str);
-        }
+  for (let item of data) {
+    const str = String(item);
+    if (!isNaN(str)) {
+      const num = parseInt(str);
+      sum += num;
+      (num % 2 === 0 ? even : odd).push(str);
+    } else if (/^[a-zA-Z]+$/.test(str)) {
+      alphabets.push(str.toUpperCase());
+      charArray.push(...str);
+    } else {
+      specialChars.push(str);
     }
+  }
 
-    let result='';
+  const reversed = charArray.reverse();
+  let concat = '';
+  for (let i = 0; i < reversed.length; i++) {
+    concat += i % 2 === 0
+      ? reversed[i].toUpperCase()
+      : reversed[i].toLowerCase();
+  }
 
-    let reversed=concat.split('').reverse();
-    for(let i=0;i<reversed.length;i++){
-        result+=i%2==0 ? reversed[i].toUpperCase() :reversed[i].toLowerCase();
-    }
-
-    res.status(200).json({
-        is_success:true,
-        user_id: "kritica_jain_09102004",
-        email: "kritica523.be22@chitkara.edu.in",
-        roll_number: "2210990523",
-        odd_numbers: odd,
-        even_numbers: even,
-        alphabets: alphabets,
-        special_characters: specialChars,
-        sum: String(sum),
-        concat_string: result
-    });
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  return res.status(200).json({
+    is_success: true,
+    user_id: "kritica_jain_09102004",
+    email: "kritica523.be22@chitkara.edu.in",
+    roll_number: "2210990523",
+    odd_numbers: odd,
+    even_numbers: even,
+    alphabets: alphabets,
+    special_characters: specialChars,
+    sum: String(sum),
+    concat_string: concat
+  });
+};
